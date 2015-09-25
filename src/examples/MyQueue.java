9f84fb58cfc0ae8949e6d8f48ad441a756d5d3e4
PeterSchwab <peter.schwab@bfh.ch>
2015-09-25 13:24:05 +0200

    completed MyQueue

--- src/examples/MyQueue.java
+++ src/examples/MyQueue.java
@@ -2,37 +2,50 @@ package examples;
 
 public class MyQueue<E> implements Queue<E> {
 
-	private E[] stor = (E[]) new Object[3];
+	private E[] stor = (E[]) new Object[1];
 	private int len,head,tail;
+
+	private void expand(){
+		E[] newStor = (E[]) new Object[stor.length*2];
+		for (int i=0;i<len;i++) {
+			newStor[i]=stor[head++];
+			if (head==stor.length) head=0;
+		}
+		stor = newStor;
+		head=0;
+		tail=len;
+	}
 	
 	@Override
 	public void enqueue(E o) {
-		// TODO Auto-generated method stub
-
+		if (len == stor.length) expand();
+		stor[tail++] = o;
+		if (tail==stor.length) tail=0;
+		len++;
 	}
 
 	@Override
 	public E dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		if (len==0) throw new RuntimeException("empty Queue!");
+		E ret = stor[head++];
+		if (head == stor.length) head=0; // wrap around !
+		len--;
+		return ret;
 	}
 
 	@Override
 	public E head() {
-		// TODO Auto-generated method stub
-		return null;
+		return stor[head];
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return len;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return len == 0;
 	}
 
 	public static void main(String[] args) {
@@ -41,11 +54,10 @@ public class MyQueue<E> implements Queue<E> {
 		q.enqueue("b");
 		q.enqueue("c");
 		System.out.println(q.dequeue());		
+		System.out.println(q.dequeue());		
 		q.enqueue("d");
 		System.out.println(q.dequeue());		
-		System.out.println(q.dequeue());		
-		
-		
+		System.out.println(q.dequeue());
 	}
 
 }
