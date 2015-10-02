311160fb4941711a472d8b49f6936f23239451f1
PeterSchwab <peter.schwab@bfh.ch>
2015-10-02 15:20:30 +0200

    added some methods to MyTree

--- src/examples/MyTree.java
+++ src/examples/MyTree.java
@@ -2,7 +2,9 @@ package examples;
 
 import java.util.Iterator;
 
+import javafx.geometry.Pos;
 import examples.MyLinkedList.LNode;
+import examples.MyTree2.TNode;
 
 public class MyTree<E> implements Tree<E> {
 
@@ -77,14 +79,25 @@ public class MyTree<E> implements Tree<E> {
 
 	@Override
 	public Iterator<E> childrenElements(Position<E> parent) {
-		// TODO Auto-generated method stub
-		return null;
+		TNode n = castToTNode(parent);
+		return new Iterator<E>() {
+			Iterator<TNode> it = n.children.elements();
+			@Override
+			public boolean hasNext() {
+				return it.hasNext();
+			}
+
+			@Override
+			public E next() {
+				return it.next().elem;
+			}
+		};
 	}
 
 	@Override
 	public int numberOfChildren(Position<E> parent) {
-		// TODO Auto-generated method stub
-		return 0;
+		TNode n = castToTNode(parent);
+		return n.children.size();
 	}
 
 	@Override
@@ -124,10 +137,20 @@ public class MyTree<E> implements Tree<E> {
 
 	@Override
 	public void remove(Position<E> p) {
-		// TODO Auto-generated method stub
-
+		TNode n = castToTNode(p);
+		if (n.children.size() != 0) throw new RuntimeException("cannot remove node with children!");
+		n.creator = null;
+		size--;
+		if (n == root) root = null;
+		else n.parent.children.remove(n.childrenPos);
 	}
 
+	public void removeSubtree(Position<E> p){
+		Iterator<Position<E>> it = childrenPositions(p);
+		while (it.hasNext()) removeSubtree(it.next());	
+		remove(p);
+	}
+	
 	@Override
 	public boolean isExternal(Position<E> p) {
 		// TODO Auto-generated method stub
@@ -142,14 +165,57 @@ public class MyTree<E> implements Tree<E> {
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
+
+	public void print(){
+		if (root != null)
+		print(root,"");	
+	}
+	
+	
+	private void print(Position<E> r, String ind) {
+		System.out.println(ind+r.element());
+		Iterator<Position<E>> it = childrenPositions(r);
+		while (it.hasNext()) print(it.next(),ind+"..");
+	}
+
 
 	@Override
 	public E replaceElement(Position<E> p, E o) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public String toString(){
+		if (size==0) return "";
+		StringBuilder sb = new StringBuilder("--- begin tree ---\n");
+		buildString(root,"",sb);
+		sb.append("---  end tree  ---");
+		return sb.toString();
+	}
+
+	private void buildString(Position<E> r, String in, StringBuilder sb) {
+		sb.append(in);
+		sb.append(r.element().toString());
+		sb.append("\n");
+		Iterator<Position<E>> it = childrenPositions(r);
+		while(it.hasNext()) buildString(it.next(),".."+in,sb);
+	}
+
+	public int height(){
+		return height(root);
+	}
+
+
+	private int height(Position<E> r) {
+		int h=0;
+		Iterator<Position<E>> it = childrenPositions(r);
+		while(it.hasNext()){
+			int height = height(it.next());
+			if(height > h) h=height ;
+		}
+		return h+1;
 	}
 
 	public static void main(String[] args) {
@@ -159,7 +225,33 @@ public class MyTree<E> implements Tree<E> {
 		Position<String> k2 = t.addChild(tit,"Kapitel 2");
 		Position<String> k3 = t.addChild(tit,"Kapitel 3");
 		Position<String> k1u1 = t.addChild(k1,"Abschnitt 1");
-
+		t.addChild(k1,"Abschnitt 2");
+		t.addChild(k1,"Abschnitt 3");
+		t.addChild(k1,"Abschnitt 4");
+		t.addChild(k1,"Abschnitt 5");
+//		t.print();
+		System.out.println(t);
+//		System.out.println(t.height());
+//		System.out.println(t.maxMultPos().element());
+		System.out.println(deepestPos(t).element());
 	}
 
+	static Position deepest;
+	static int maxDepth;
+	
+	static Position deepestPos(Tree t){
+		deepest = t.root();
+		if (t.size() == 0) return null;
+		deepestPos(t,t.root(),0);
+		return deepest;
+	}
+	
+	static void deepestPos(Tree t, Position p,int lev){
+		if (lev > maxDepth){
+			maxDepth = lev;
+			deepest = p; 
+		}		
+		Iterator<Position> it = t.childrenPositions(p);
+		while(it.hasNext()) deepestPos(t,it.next(),lev+1);
+	}
 }
