67cd1b6bcace2d067ceda5cf155f530c7eba1888
PeterSchwab <peter.schwab@bfh.ch>
2015-10-20 19:04:57 +0200

    added some details to MyAVLTree

--- src/examples/MyAVLTree.java
+++ src/examples/MyAVLTree.java
@@ -140,9 +140,22 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 		}
 		n.expand(key,o);
 		size++;
+		adjustHeightAboveAndRebalance(n);
 		return n;
 	}
 
+
+
+
+	private void adjustHeightAboveAndRebalance(AVLNode n) {
+		n = n.parent;
+		while (n != null ){
+			int newHeight = Math.max(n.left.height,n.right.height)+1;
+			if (newHeight == n.height) break;
+			n.height = newHeight;
+			n = n.parent;
+		}
+	}
 
 
 
@@ -163,14 +176,11 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 
 
 
-
 	@Override
 	public Locator<K, E> closestAfter(K key) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
 
 
 	@Override
@@ -224,7 +234,7 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 	}
 	
 	public void print(){
-		if (size>0) print(root,"");
+		if (size>0) prittyPrint(root,"");
 	}
 
 	private void print(AVLNode n,String in) {
@@ -247,7 +257,7 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 		else inN = in+"-"; // root of the tree
 		if ( ! r.right.isExternal()) System.out.println(inNeu+" |");
 		else System.out.println(inNeu);
-		System.out.println(inN+r.key());//+"(h="+r.height+")"+":"+r.elem+")"); 
+		System.out.println(inN+r.key()+"(h="+r.height+")"+":"+r.elem+")"); 
 		// left subtree
 		inNeu = in;
 		if (r.isLeftChild()){
@@ -256,47 +266,66 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 		prittyPrint(r.left,inNeu+" |");
 	}
 
+	public void test(){
+		test(root);
+	}
+
+	private void test(AVLNode n) {
+		if (n.isExternal()) return;
+		test(n.left);
+		test(n.right);
+		if (n.left.parent != n) throw new RuntimeException("chaining incorrect"+n.key);
+		if (n.right.parent != n) throw new RuntimeException("chaining incorrec"+n.key);
+		if (Math.max(n.left.height,n.right.height)+1!=n.height)
+			throw new RuntimeException("Height wrong"+n.key);
+		if (n.left.key !=null &&  n.left.key.compareTo(n.key)>0) throw new RuntimeException("order wrong"+n.key); 
+		if (n.right.key !=null &&  n.right.key.compareTo(n.key)<0) throw new RuntimeException("order wrong"+n.key); 
+	}
+
+
+
 	public static void main(String[] argv){
 		MyAVLTree<Integer, String> t = new MyAVLTree<>();
-		Random rand = new Random(3463453);
-		t.insert(6, "e 6.1");
-		t.insert(10, "e 10");
-		t.insert(7, "e 7");
-		t.insert(6, "e 6.2 ");
-		t.insert(3, "e 3");
-		Locator loc = t.insert(1, "e 1");		
-		t.insert(6, "e 6.3 ");
-		t.insert(4, "e 4");
-		t.insert(11, "e 11");
-		// t.print();
-		while (loc != null) {
-			System.out.println(loc.key());
-			loc = t.next(loc);
-		}
-		System.out.println(t.find(6).element());
-		int n  = 1000000;
+		Random rand = new Random();
+//		t.insert(6, "e 6.1");
+//		t.insert(10, "e 10");
+//		t.insert(7, "e 7");
+//		t.insert(6, "e 6.2 ");
+//		t.insert(3, "e 3");
+//		Locator loc = t.insert(1, "e 1");		
+//		t.insert(6, "e 6.3 ");
+//		t.insert(4, "e 4");
+//		t.insert(11, "e 11");
+//		t.print();
+//		while (loc != null) {
+//			System.out.println(loc.key());
+//			loc = t.next(loc);
+//		}
+//		System.out.println(t.find(6).element());
+		int n  = 10;
 		Locator<Integer,String>[] locs = new Locator[n];
 		long time1 = System.nanoTime();
 		for (int i=0;i<n;i++) {
-			 locs[i]=t.insert(rand.nextInt(n),""+i);
+			 locs[i]=t.insert(rand.nextInt(n),"e"+i);
 			//locs[i]=t.insert(i, "bla");
 		}
+		t.test();
 		// for (int i= 0; i<n/2; i++ ) t.remove(locs[i]);
 		long time2 = System.nanoTime(); 
 		System.out.println("ms used: "+(time2-time1)/1e6);
 		System.out.println(t.root.height);
 		// System.out.println((t.find(13).element()));
-		// t.print();
-		// Locator<Integer,String> 
-		loc = t.min();
-		while (loc != null){
-			System.out.println(loc.key());
-			loc = t.next(loc);
-		}
-	}
-//	Map s = new TreeMap();
+		 t.print();
+//		Locator<Integer,String> 
+//		loc = t.min();
+//		while (loc != null){
+//			System.out.println(loc.key());
+//			loc = t.next(loc);
+//		}
+//	}
+////	Map s = new TreeMap();
 //	for (int i=0;i<n;i++) {
 //		s.put(rand.nextInt(),"");
 //	}
-//}
+	}
 }
