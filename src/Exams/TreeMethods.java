2b92f10a04aaa64aa2bc1e1638627eabb11ed71f
Denis Shevchenko <mail@shevchenko.ch>
2016-01-19 00:24:42 +0100

    Added findDistance() between two nodes

--- src/Exams/TreeMethods.java
+++ src/Exams/TreeMethods.java
@@ -44,6 +44,38 @@ public class TreeMethods {
         }
     }
 
+    private static int distRecursive(BTree t, Position root, Position to, int level){
+
+        if(root == to)
+            return level;
+
+        if(root != null){
+            level++;
+            if(t.leftChild(root) != null)
+                distRecursive(t, t.leftChild(root), to, level++);
+            if(t.rightChild(root) != null)
+                distRecursive(t, t.rightChild(root), to, level++);
+        }
+
+        return level;
+    }
+
+    private static Position LCA(BTree tree, Position root, Position p1, Position p2){
+        if(root == null)
+            return null;
+
+        if(root == p1 || root == p2)
+            return root;
+
+        Position leftElement = LCA(tree, tree.leftChild(root), p1, p2);
+        Position rightElement = LCA(tree, tree.rightChild(root), p1, p2);
+
+        if(leftElement != null && rightElement != null)
+            return root;
+
+        return (leftElement != null) ? leftElement : rightElement;
+    }
+
     public static int breadth(BTree t){
         int level = 0;
         int[] count = new int[t.size()];
@@ -51,5 +83,16 @@ public class TreeMethods {
         getBreadthRecursively(t.root(), count, level, t);
 
         return getMax(count);
+    }
+
+    public static int dist(BTree t, Position from, Position to){
+        int level = 0;
+        Position lca = LCA(t, t.root(), from, to);
+
+        int distPos1 = distRecursive(t, t.root(), from, level);
+        int distPos2 = distRecursive(t, t.root(), to, level);
+        int distLCA = distRecursive(t, t.root(), lca, level);
+
+        return (distPos1 + distPos2) - 2*distLCA;
     }
 }
